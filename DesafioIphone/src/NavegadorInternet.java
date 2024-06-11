60b99af407dab3b45bcc7c8528a9367c622931a4
Thiago Campos <thiago.campos715@gmail.com>
2024-06-11 00:44:55 -0300

    Cirando as interfaces

---
+++ DesafioIphone/src/NavegadorInternet.java
@@ -0,0 +1,5 @@
+public interface NavegadorInternet {
+    public void exibirpagina(String url);
+    public void adicionarNovaAba();
+    public void atualizarPagina();    
+}
---
+++ DesafioIphone/src/ReprodutorMusical.java
@@ -0,0 +1,5 @@
+public interface ReprodutorMusical {
+    public void selecionarMusica(String musica);
+    public void tocar();
+    public void pausar();
+}
