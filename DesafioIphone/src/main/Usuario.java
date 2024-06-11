133f6321aff798ccf171c7b54ec29b1ee75c7956
Thiago Campos <thiago.campos715@gmail.com>
2024-06-11 01:05:28 -0300

    Criando a classe main e executando o desafio

--- DesafioIphone/src/main/Usuario.java
+++ DesafioIphone/src/main/Usuario.java
@@ -1,7 +1,26 @@
 package main;
 
+import classes.Iphone;
+
 public class Usuario {
     public static void main(String[] args) {
+        Iphone iphone = new Iphone();
         
+        System.out.println("Reprodutor Musical");
+        iphone.selecionarMusica(" Simple Man-Lynyrd Skynyrd");
+        iphone.pausar();
+        iphone.tocar();
+        System.out.println("\n");
+
+        System.out.println("Aparelho Telefônico");
+        iphone.ligar(" 119XXXXXXXX");
+        iphone.atender();
+        iphone.iniciarCorreioVoz();
+        System.out.println("\n");
+        
+        System.out.println("Navegador Internet");
+        iphone.exibirpagina(" www.github.com");
+        iphone.atualizarPagina();
+        iphone.adicionarNovaAba();
     }
 }
