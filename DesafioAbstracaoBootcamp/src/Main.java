3cd7862b55fa353b89b599b6d34062011d4bda67
Thiago Campos <thiago.campos715@gmail.com>
2024-07-25 18:22:26 -0300

    Criando as classes Curso e Mentoria

---
+++ DesafioAbstracaoBootcamp/src/Main.java
@@ -0,0 +1,18 @@
+import dominio.Curso;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Java");
+        curso1.setDescricao("Descrição Java");
+        curso1.setCargaHoraria(4);
+        
+        Curso curso2 = new Curso();
+        curso2.setTitulo("JavaScript");
+        curso2.setDescricao("Descrição JavaScript");
+        curso2.setCargaHoraria(4);
+        
+        System.out.println(curso1);
+        System.out.println(curso2);
+    }
+}
